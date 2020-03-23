084fd7c88cc063401b48a801cde9b1fc867ddd08
alexandr-tg <5b4e5f58555f4e5b@gmail.com>
2020-03-23 06:18:08 +0200

    database settings change

--- ajax/connect/DataBase.php
+++ ajax/connect/DataBase.php
@@ -4,8 +4,8 @@ namespace ajax\connect;
 
 class DataBase
 {
-    const DB_HOST = '127.0.0.1';
-    const DB_NAME = 'tasker_bd';
-    const DB_USER = 'root';
-    const DB_PASS = '';
+    const DB_HOST = 'us-cdbr-iron-east-04.cleardb.net';
+    const DB_NAME = 'heroku_7c10a2a5920e6f3';
+    const DB_USER = 'bd3929f4db520a';
+    const DB_PASS = 'b28f4c56';
 }
